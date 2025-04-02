a772dfad3c90cf89412fe2c0764e4686f79d0e62
TurikRoma <turikroma15@gmail.com>
2025-04-02 16:52:07 +0300

    update

--- app/generators.py
+++ app/generators.py
@@ -46,7 +46,7 @@ async def mental_analysis_gpt(context):
             json={
                 # "system": context,
                 "model": "claude-3-5-haiku-20241022",  # Убедитесь, что указываете правильную модель
-                "max_tokens": 4000,
+                "max_tokens": 180,
                 "messages": [{"role": "user", "content": context}],
             }
         )
